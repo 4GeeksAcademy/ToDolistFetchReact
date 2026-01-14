51f5cd7a8caf0bc19b4d625137927eceef674cf8
alejandrolobatogil-dot <alejandrolobatogil@gmail.com>
2026-01-14 19:33:49 +0000

    Terminado1

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,100 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
+	const [user, setUser] = useState(null)
+	const [input, setInput] = useState("")
+
+	async function getData() {
+		try {
+			let response = await fetch("https://playground.4geeks.com/todo/users/Alexander")
+			let data = await response.json()
+			setUser(data)
+			console.log(data)
+		} catch (error) {
+			console.log(error)
+		}
+	}
+	async function postTask(event) {
+		event.preventDefault()
+		const newTask = {
+			"label": input,
+			"is_done": false,
+
+
+		}
+		await fetch("https://playground.4geeks.com/todo/todos/Alexander", {
+			method: "POST",
+			body: JSON.stringify(newTask),
+			headers: { "Content-Type": "application/json" }
+		})
+		getData()
+
+
+	}
+
+
+
+	async function deleteTask(id) {
+		await fetch(`https://playground.4geeks.com/todo/todos/${id}`, {
+			method: "DELETE",
+		})
+		getData()
+	}
+	async function putTask(element) {
+		let task = {
+			"label": element.label,
+			"is_done": !element.is_done
+		}
+		await fetch(`https://playground.4geeks.com/todo/todos/${element.id}`, {
+			method: "PUT",
+			body: JSON.stringify(task),
+			headers: { "Content-Type": "application/json" }
+		})
+		getData()
+
+	}
+
+	useEffect(() => {
+		getData()
+	}, [])
+
+
 	return (
 		<div className="text-center">
-            
+			<h1>{user ? user.username : "cargando"}</h1>
+			<form onSubmit={postTask}>
+				<input
+					type="text"
+					onChange={(e) => setInput(e.target.value)}
+					value={input}
+					placeholder="Limpiar la cocina"
+				/>
+				<button type="submit">
+					Añadir tarea
+				</button>
+			</form>
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			{user?.todos?.map((e) => {
+				return (
+					<div className="task" key={e.id}>
+						<span>
+							{e.label}
+							<div className="completoincompleto"onClick={() => putTask(e)}>
+								{e.is_done ? "Completo" : "Incompleto"}
+							</div>
+						</span>
+						<span onClick={() => deleteTask(e.id)}>x</span>
+					</div>
+				);
+			})}
+
+			{!user && <span>Cargando...</span>}
 		</div>
 	);
+
 };
 
 export default Home;
